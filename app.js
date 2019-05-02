4fb5c7062e6a91dd60fd3b853c337bf947b26913
Cfarchmin <46389012+Cfarchmin@users.noreply.github.com>
2019-05-01 20:38:04 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,86 +1,73 @@
 function buildMetadata(sample) {
 
-// @TODO: Complete the following function that builds the metadata panel
+  // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  d3.json(`/metadata/${sample}`).then((sampleData) => {
+  console.log(sampleData)
     // Use d3 to select the panel with id of `#sample-metadata`
-    var metadata = d3.select("#sample-metadata");
-
+    var PANEL = d3.select("#sample-metadata");
     // Use `.html("") to clear any existing metadata
-    metadata.html("");
-  
+    PANEL.html("")
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-    var url = `/metadata/${sample}`;
-    d3.json(url).then((response) => {
-      Object.entries(response).forEach(d => {
-        metadata.append('p').text(`${d[0]}: ${d[1]}`);
-      });
-  
-    });
-
+    Object.entries(sampleData).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key}: ${value}`);
+    })
+  })
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-
 }
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  pie_div = document.getElementById('pie');
+  d3.json(`/samples/${sample}`).then((sampleData) => {
+    console.log(sampleData)
+    const otu_ids = sampleData.otu_ids;
+    const otu_labels = sampleData.otu_labels;
+    const sample_values = sampleData.sample_values;
+    // @TODO: Build a Bubble Chart using the sample data
 
-  var url = `/samples/${sample}`;
-
-  d3.json(url).then((response) => {
-
-  // @TODO: Build a Bubble Chart using the sample data
-    var trace = {
-      x: response['otu_ids'],
-      y: response['sample_values'],
-      hovertext: response['otu_labels'],
-      hoverinfo: 'hovertext',
-      mode: 'markers',
+    var bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
       marker: {
-        size: response['sample_values'],
-        color: response['otu_ids']
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Earth"
       }
+    }];
+
+    var bubbleLayout = {
+      margin: { t: 0},
+      hovermode: "closest",
+      xaxis: {title: "OTU ID"}
     };
-    
-    var data = [trace];
-    
-    var layout = {
-      title: 'Bubble Chart',
-      showlegend: false,
-      height: 600,
-      width: 1200,
-      // margin: {t: 0},
-      xaxis: { title: "OTU ID"},
-      yaxis: { title: "Values"}
-    };
-    
-    Plotly.newPlot('bubble', data, layout);
+
+    Plotly.plot("bubble", bubbleData, bubbleLayout)
 
     // @TODO: Build a Pie Chart
+    var pieData = [{
+      values: sample_values.slice(0,10),
+      labels: otu_ids.slice(0,10),
+      hovertext: otu_labels.slice(0,10),
+      hoverinfo: "hovertext",
+      type: "pie"
+    }];
+
+    var pieLayout = {
+      margin: { t: 0, 1:0}
+    }
+
+    Plotly.plot("pie", pieData, pieLayout)
+
+  })
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-
-    var trace1 = {
-      labels: response['otu_ids'].slice(0,10),
-      values: response['sample_values'].slice(0,10),
-      hovertext: response['otu_labels'].slice(0,10),
-      hoverinfo: 'hovertext',
-      type: "pie"
-    };
-
-    var data = [trace1];
-
-    var layout = {
-      title: "Pie Chart",
-    };
-
-    Plotly.newPlot(pie_div, data, layout);
-  });
 }
 
 function init() {
